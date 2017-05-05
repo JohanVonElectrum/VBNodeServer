ecdf88d79555ccc22074a960053a7036cd8a7086
JoanTheGamer <joanestradium@gmail.com>
2017-05-05 13:47:33 +0200

    v1.0
    The name of Node.js_Listener is a fail

---
+++ Node.js_Listener/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(508, 611)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(514, 3)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(529, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Timer1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(514, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(514, 58)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(529, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(924, 87)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Label1"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(514, 87)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(515, 117)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(528, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(514, 143)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Install"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1055, 611)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Node.js_Listener/Form1.vb
@@ -0,0 +1,108 @@
+﻿'La ruta de los archivos es D:/VBNodeServer
+'Es necesario que el archivo de node.js se llame main.js
+'Este programa esta hecho al cien por cien por JoanTheGamer
+
+
+Imports System.Text
+
+Public Class Form1
+    Dim fs As IO.FileStream,
+        file As IO.StreamWriter,
+        extensions As String
+
+    Public Function Start()
+        If My.Computer.FileSystem.FileExists("D:/VBNodeServer/start.bat") Then
+            Process.Start("CMD", "/K cd D:/VBNodeServer/ & start.bat")
+        Else
+            file = My.Computer.FileSystem.OpenTextFileWriter("D:/VBNodeServer/start.bat", True)
+            file.WriteLine(":a")
+            file.WriteLine("cls")
+            file.WriteLine("node main.js")
+            file.WriteLine("goto a")
+            file.Close()
+            Start()
+        End If
+        Return 0
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ProgressBar1.Value = 100
+        Label1.Text = "0/0"
+        If My.Computer.FileSystem.FileExists("D:/VBNodeServer/npm_Complements.txt") = False Then
+            Using fs As IO.FileStream = IO.File.Create("D:/VBNodeServer/npm_Complements.txt")
+                fs.Close()
+            End Using
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If My.Computer.FileSystem.FileExists(TextBox2.Text) Then
+            ProgressBar1.Maximum = My.Computer.FileSystem.ReadAllText(TextBox2.Text).Length
+            If ProgressBar1.Value < My.Computer.FileSystem.ReadAllText(TextBox2.Text).Length - 1 Then
+                Label1.Show()
+                Label1.Text = (ProgressBar1.Value * 58.1).ToString + "/" + My.Computer.FileSystem.ReadAllText(TextBox2.Text).Length.ToString
+                ProgressBar1.Value += 1
+            ElseIf ProgressBar1.Value = My.Computer.FileSystem.ReadAllText(TextBox2.Text).Length - 1 Then
+                Label1.Hide()
+                ProgressBar1.Value += 1
+            End If
+            If My.Computer.FileSystem.ReadAllText(TextBox2.Text).Length > 100 Then
+                ProgressBar1.Maximum = 100
+            End If
+            If TextBox1.Text = My.Computer.FileSystem.ReadAllText(TextBox2.Text) Then
+                Timer1.Stop()
+            ElseIf ProgressBar1.Value = ProgressBar1.Maximum Then
+                TextBox1.Text = My.Computer.FileSystem.ReadAllText(TextBox2.Text)
+            End If
+        Else
+            ProgressBar1.Value = ProgressBar1.Maximum
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If My.Computer.FileSystem.FileExists(TextBox2.Text) Then
+            TextBox2.ForeColor = Color.Green
+            Dim response = MsgBox("You want use this File", MsgBoxStyle.YesNo, "File Confirmation")
+            If response = MsgBoxResult.Yes Then
+                ProgressBar1.Value = 0
+                Timer1.Start()
+            Else
+                TextBox2.Text = ""
+            End If
+        Else
+            TextBox2.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        extensions = My.Computer.FileSystem.ReadAllText("D:/VBNodeServer/npm_Complements.txt")
+        If extensions.Contains(TextBox3.Text) Then
+            MsgBox("Este complemento ya esta instalado.")
+        Else
+            extensions = extensions + TextBox3.Text + ", "
+            Using fs As IO.FileStream = IO.File.Create("D:/VBNodeServer/npm_Complements.txt")
+                Dim info As Byte() = New UTF8Encoding(True).GetBytes(extensions)
+                fs.Write(info, 0, info.Length)
+                fs.Close()
+            End Using
+            Process.Start("CMD", "/K cd D:/VBNodeServer/ & npm install " + TextBox3.Text)
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text <> My.Computer.FileSystem.ReadAllText(TextBox2.Text) Then
+            Dim response = MsgBox("Save?", MsgBoxStyle.YesNo, "Save Confirmation")
+            If response = MsgBoxResult.Yes Then
+                Using fs As IO.FileStream = IO.File.Create(TextBox2.Text)
+                    Dim info As Byte() = New UTF8Encoding(True).GetBytes(TextBox1.Text)
+                    fs.Write(info, 0, info.Length)
+                    fs.Close()
+                End Using
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Start()
+    End Sub
+End Class
